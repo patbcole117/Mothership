6e41f7bf916f8014fbd86e1dc44a14823550f9c9
patbcole117 <pcolemanofficial@gmail.com>
2023-05-09 10:44:05 +0000

    First Commit!

---
+++ squid.py
@@ -0,0 +1,19 @@
+import http.client
+
+DEFAULT_MOTHERSHIP_IP = "127.0.0.1"
+DEFAULT_MOTHERSHIP_PORT = 8000
+
+
+def main():
+    mission_plan = phone_home(DEFAULT_MOTHERSHIP_IP, DEFAULT_MOTHERSHIP_PORT)
+    print(mission_plan)
+
+def phone_home(mothership_ip: str, mothership_port: int):
+    conn = http.client.HTTPConnection(mothership_ip, mothership_port)
+    conn.request("GET", "/")
+    r = conn.getresponse()
+    return r.read().decode()
+
+
+if __name__ == "__main__":
+    main()
