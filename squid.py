bceaa17e95fc12acd5c337c36695c16e6ea75713
patbcole117 <pcolemanofficial@gmail.com>
2023-05-10 09:36:14 +0000

    threading http server loop functioning.

--- squid.py
+++ squid.py
@@ -9,13 +9,18 @@ DEFAULT_MOTHERSHIP_PORT = 8000
 LOCAL_ADDRESS = "127.0.0.1"
 LOCAL_PORT = 8001
 
+DEFAULT_DURATION = 5
 
 def main():
-    for i in range(5):
-        print(f'ITERATION: {i}\n')
+    local_channel_thread = open_local_channel(LOCAL_ADDRESS, LOCAL_PORT)
+    while True:
         mission_plan = phone_home(DEFAULT_MOTHERSHIP_ADDRESS, DEFAULT_MOTHERSHIP_PORT)
         print(mission_plan)
-        open_local_channel(LOCAL_ADDRESS, LOCAL_PORT)
+        if local_channel_thread.is_alive():
+            print('LOCAL CHANNEL IS OPEN')
+        else:
+            local_channel_thread = open_local_channel(LOCAL_ADDRESS, LOCAL_PORT)
+        time.sleep(DEFAULT_DURATION)
         
 
 def phone_home(mothership_ip: str, mothership_port: int):
@@ -23,20 +28,25 @@ def phone_home(mothership_ip: str, mothership_port: int):
     print('PHONE HOME...')
 
     conn = http.client.HTTPConnection(mothership_ip, mothership_port)
-    conn.request("GET", "/")
-    r = conn.getresponse()
-    return r.read().decode()
+    try:
+        conn.request("GET", "/")
+        r = conn.getresponse()
+        return r.read().decode()
+    except ConnectionRefusedError as err:
+        print(err)
+        print('CONNECTION TO MOTHERSHIP REFUSED.')
+
 
 def open_local_channel(local_ip: str, local_port: int, duration: int = 5):
     
-    print(f'OPENING LOCAL CHANNEL FOR {duration} SECONDS...')
+    print(f'CREATING NEW LOCAL CHANNEL THREAD...')
 
-    timeout = time.time() + duration
-    server = ThreadingHTTPServer((LOCAL_ADDRESS, LOCAL_PORT), LocalHandler)
-    while time.time() < timeout:
-        server.handle_request()
-
-    print(f'CLOSING LOCAL CHANNEL...')
+    local_channel_server = ThreadingHTTPServer((LOCAL_ADDRESS, LOCAL_PORT), LocalHandler)
+    local_channel_thread = threading.Thread(target=local_channel_server.serve_forever)
+    local_channel_thread.daemon = True
+    local_channel_thread.start()
+    
+    return local_channel_thread
 
 
 class LocalHandler(BaseHTTPRequestHandler):
