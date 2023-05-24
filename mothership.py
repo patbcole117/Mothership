0917fba434f552b8efd779cc58d5235884939b14
patbcole117 <pcolemanofficial@gmail.com>
2023-05-24 11:53:48 +0000

    checkpoint

--- mothership.py
+++ mothership.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from mdc import get_database, insert_document
+from mdc import get_database, insert_document, find_document
 import time
 
 class Squid(BaseModel):
-    id: str
-    group: str
+    sid: str
+    gid: str = "SQUIDS"
     hostname: str
     mac: int
     timestamp: float = time.time()
 
-    def to_mongo(self):
-        return {"_id": self.id, "group": self.group, "hostname": self.hostname, "mac": self.mac, "timestamp": self.timestamp}
-
-
 class Call(BaseModel):
-    squid_id: str
-    qcommand_id: str
+    sid: str
+    cid: str
     result: str
     timestamp: float = time.time()
 
-# curl -Uri http://127.0.0.1:8000/admin/qcommand -Method POST -Body $post -ContentType 'application/json'
-
-class Qcommand(BaseModel):
+#curl -Uri http://127.0.0.1:8000/admin/command -Method POST -Body $post -ContentType 'application/json'
+#$post = @{                                                                            
+#target_id = "123456"
+#mid = 1
+#payload = "Hello world!"
+#} | ConvertTo-Json
+class Command(BaseModel):
     target_id: str
-    module_id: int
+    mid: int
     payload: str
     created: float = time.time()
     status: str = "QUEUED"
@@ -41,17 +41,21 @@ async def root():
 
 @app.post("/squids/home/")
 def squids_checkin(call: Call):
-    response = insert_document("calls", vars(call))
-    return {"message": response}
+    insert_document("calls", vars(call))
+    squid = find_document("squids", "sid", call.sid)
+    sid = squid['sid']
+    gid = squid['gid']
+    command = find_document("commands", "target_id", gid)
+    return {command}
 
 
 @app.post("/squids/register/")
 def squids_registration(squid: Squid):
-    response = insert_document("squids", squid.to_mongo())
+    response = insert_document("squids", vars(squid))
     return {"message": response}
 
 
-@app.post("/admin/qcommand")
-def admin_qcommand(qcommand: Qcommand):
-    response = insert_document("qcommands", vars(qcommand))
+@app.post("/admin/command")
+def admin_qcommand(command: Command):
+    response = insert_document("commands", vars(command))
     return {"message": response}
