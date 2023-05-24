8f8b0fb5aee45228fc122cc4c4bd890ba1acf6bd
patbcole117 <pcolemanofficial@gmail.com>
2023-05-24 08:40:49 +0000

    classes for call and qcommand

--- mothership.py
+++ mothership.py
@@ -14,14 +14,19 @@ class Squid(BaseModel):
 
 
 class Call(BaseModel):
-    caller: str
+    squid_id: str
+    qcommand_id: str
+    result: str
     timestamp: float
 
 
-class Order(BaseModel):
-    to: str
-    command: str
-    issued: bool
+class Qcommand(BaseModel):
+    target_id: str
+    module: int
+    payload: str
+    created: float
+    status: str
+    # status = [QUEUED, ISSUED, FAILED, COMPLETED]
 
 
 app = FastAPI()
@@ -43,3 +48,8 @@ def squids_registration(squid: Squid):
     response = insert_document("squids", squid.to_mongo())
     return {"message": response}
 
+
+@app.post("/admin/qcommand")
+def admin_qcommand(qcommand: Qcommand):
+    response = insert_document("qcommands", vars(qcommand))
+    return {"message": response}
